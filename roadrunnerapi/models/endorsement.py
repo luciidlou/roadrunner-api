9688ab6ad90b1be8230efa46311dda3e018978d2
luciidlou <lewisale96@gmail.com>
2022-03-10 15:01:22 -0600

    implemented seed data in fixtures directory

--- roadrunnerapi/models/endorsement.py
+++ roadrunnerapi/models/endorsement.py
@@ -2,4 +2,5 @@ from django.db import models
 
 
 class Endorsement(models.Model):
+    letter = models.CharField(max_length=1)
     label = models.CharField(max_length=80)
--- roadrunnerapi/models/freight_type.py
+++ roadrunnerapi/models/freight_type.py
@@ -3,3 +3,4 @@ from django.db import models
 
 class FreightType(models.Model):
     label = models.CharField(max_length=80)
+    endorsement = models.ForeignKey("Endorsement", on_delete=models.CASCADE, null=True)
