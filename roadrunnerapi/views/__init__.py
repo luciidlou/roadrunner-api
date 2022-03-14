63aac3363b8d59ca8eb9e8af1e5635e5a1f03462
luciidlou <lewisale96@gmail.com>
2022-03-14 16:53:42 -0500

    altered datetime fields on Load model

--- roadrunnerapi/views/__init__.py
+++ roadrunnerapi/views/__init__.py
@@ -1,4 +1,4 @@
-from .auth_view import login_user, register_user
-from .freight_type_view import FreightTypeView
+from .auth_view import register_user, login_user
 from .load_view import LoadView
 from .truck_view import TruckView
+from .freight_type_view import FreightTypeView
